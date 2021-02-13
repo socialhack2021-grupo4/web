74a4ac54e4f939d96264c714c88fecf9a986517d
Gabriel <furstenheim@gmail.com>
2021-02-13 13:05:18 +0100

    Access db for big endpoint

---
+++ server/initial-data/experiences.js
@@ -0,0 +1,38 @@
+const _ = require('lodash')
+module.exports = [
+  {
+    id: 1,
+    host: {
+      id: 1,
+      profile_pic: 'http://',
+      instagram: 'http://'
+    },
+    ngo: {
+      id: 1,
+      icon: 'http',
+      url: 'http'
+    },
+    title: 'Cena en mi casa',
+    description: 'Disfruta en la nueva casa de ibai...',
+    // To be shown on the detail
+    full_description: _.times('Loren ipsum', 40),
+    pic_thumbnail_url: 'http://',
+    pic_url: 'http://',
+    // For the carousel
+    pics: [{
+      url: 'http://'
+      },
+      {
+      url: 'http://'
+      }
+    ],
+    is_bought: null, // Depends on user
+    starred: null, // Depends on user
+    n_participants: 10,
+    priority: 0.8, // low 0 < priority < 1 top
+    date_start: '2020-10-10 10:10',
+    date_end: '2021-10-10 10:10',
+    date_at: '2021-12-10 10:10',
+    price: 1234 // 12,34
+  }
+]
---
+++ server/initial-data/users.js
@@ -0,0 +1,16 @@
+const users = [
+  {
+    id: 1,
+    bought_experiences: [
+      {
+        id: 1
+      },
+      {
+        id: 2
+      }
+    ],
+    profile_pic: 'http://'
+  }
+]
+
+module.exports = users
